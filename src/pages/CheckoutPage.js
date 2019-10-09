b12075b4b31dd9d36c3b03bd3456a74333c5a978
Ashuanuj <ashutoshchoubeyanuj@gmail.com>
2019-10-09 12:43:08 +0530

    changes regarding errors

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row,Media,Table,FormGroup,Label,CustomInput,Button,Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Row, Media, Table, FormGroup, Label, CustomInput, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Page from '../components/Page'
 
 import vegImg from '../components/assets/img/icons/veg.png';
@@ -11,94 +11,101 @@ import { connect } from 'react-redux';
 
 import { orderRequest } from '../actions'
 
-class CheckoutPage extends React.Component{
+class CheckoutPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          modal: false
+            modal: false
         };
-    
+
         this.toggle = this.toggle.bind(this);
-      }
-    
-      toggle() {
+        this.anotherToggle = this.toggle.bind(this);
+    }
+
+    toggle() {
         this.setState(prevState => ({
-          modal: !prevState.modal
+            modal: !prevState.modal
         }));
-        let _data=this.props.cartItems
+    }
+
+    anotherToggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+        let _data = this.props.cartItems
         this.props.actions.orderRequest(_data);
-      }
+    }
 
     //   componentWillMount() {
     //     this.props.actions.checkoutPage();
     //   }
 
-      
-    render(){
-        const {props} = this;
+
+    render() {
+        const { props } = this;
         console.log(props, 'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')
 
-        return(
+        return (
 
             <div>
-               <Page>
-                <Row className="checkout-div"> 
-                    <Table responsive className="TableMainList" >
-                        <tbody className="t-body">
-                        <tr>
-                            <td>  
-                               <Media object src={vegImg} alt="image"/> Idly Vada
+                <Page>
+                    <Row className="checkout-div">
+                        <Table responsive className="TableMainList" >
+                            <tbody className="t-body">
+                                <tr>
+                                    <td>
+                                        <Media object src={vegImg} alt="image" /> Idly Vada
                             </td>
-                            <td>  
-                                <div className="qtybtn"> 
-                                    <span className="minus">-</span>
-                                    {/* <span className="count"><input value="2"/></span> */}
-                                    <span className="count">2</span>
-                                    <span className="plus">+</span>
-                                </div>
+                                    <td>
+                                        <div className="qtybtn">
+                                            <span className="minus">-</span>
+                                            {/* <span className="count"><input value="2"/></span> */}
+                                            <span className="count">2</span>
+                                            <span className="plus">+</span>
+                                        </div>
+                                    </td>
+                                    <td> $4.00 </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Media object src={vegImg} alt="image" /> Bread Toast
                             </td>
-                            <td> $4.00 </td>
-                        </tr>
-                        <tr>
-                            <td>  
-                               <Media object src={vegImg} alt="image"/> Bread Toast
+                                    <td>-</td>
+                                    <td> $4.00 </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Media object src={NonvegImg} alt="image" /> Omelette
                             </td>
-                            <td>-</td>
-                            <td> $4.00 </td>
-                        </tr>
-                        <tr>
-                            <td>  
-                                 <Media object src={NonvegImg} alt="image"/> Omelette 
-                            </td>
-                            <td>-</td>
-                            <td> $3.00 </td>
-                        </tr>
-                        </tbody>
-                    </Table>
-                    <Table className="tableRadio">
-                        <tbody className="radio-div">
-                            <tr>
-                            <FormGroup row className="table-div">
-                            <Label className="label" for="exampleCheckbox">Select a Slot : </Label>
-                                <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Now" inline defaultChecked/>
-                                <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="Later" inline/>
-                            </FormGroup>
-                            </tr>
-                        </tbody>
-                    </Table>
-                    <div className="note-text-checkout"><span>Instructions? E.g. Don’t ring the doorbell</span> </div>
-                  
-                    <Table className="bill-amt">
-                        <tbody className="radio-div">
-                            <tr>
-                                <td className="totaltext">Total Bill</td>
-                                <td>-</td>
-                                <td className="totalamt"> $11.00 </td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                   
+                                    <td>-</td>
+                                    <td> $3.00 </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        <Table className="tableRadio">
+                            <tbody className="radio-div">
+                                <tr>
+                                    <FormGroup row className="table-div">
+                                        <Label className="label" for="exampleCheckbox">Select a Slot : </Label>
+                                        <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Now" inline defaultChecked />
+                                        <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="Later" inline />
+                                    </FormGroup>
+                                </tr>
+                            </tbody>
+                        </Table>
+                        <div className="note-text-checkout"><span>Instructions? E.g. Don’t ring the doorbell</span> </div>
+
+                        <Table className="bill-amt">
+                            <tbody className="radio-div">
+                                <tr>
+                                    <td className="totaltext">Total Bill</td>
+                                    <td>-</td>
+                                    <td className="totalamt"> $11.00 </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+
                         <div className="confirmBtn-div">
                             <Button
                                 size="lg"
@@ -106,23 +113,23 @@ class CheckoutPage extends React.Component{
                                 block
                                 onClick={this.toggle}
 
-                                >
-                                    Confirm Order
+                            >
+                                Confirm Order
                             </Button>
                         </div>
-                
-                 </Row> 
+
+                    </Row>
                 </Page>
 
-{/* confirmation modal */}
+                {/* confirmation modal */}
                 <Modal isOpen={this.state.modal} toggle={this.toggle} className="modal-div">
-                <ModalHeader toggle={this.toggle}>Confirmation Message</ModalHeader>
-                <ModalBody>
-                     Body Copy Message
+                    <ModalHeader toggle={this.toggle}>Confirmation Message</ModalHeader>
+                    <ModalBody>
+                        Body Copy Message
                 </ModalBody>
-                <ModalFooter>
-                    <Button className="okBtn bg-gradient-Requestbtn border-0" onClick={this.toggle}>OK</Button>{' '}
-                </ModalFooter>
+                    <ModalFooter>
+                        <Button className="okBtn bg-gradient-Requestbtn border-0" onClick={this.anotherToggle}>OK</Button>{' '}
+                    </ModalFooter>
                 </Modal>
             </div>
         );
@@ -135,24 +142,22 @@ CheckoutPage.propTypes = {
     checkout: PropTypes.bool.isRequired,
 };
 
-function mapStateToProps(state)
-{
+function mapStateToProps(state) {
     return {
-          checkout: state.checkoutReducers.checkout,
-          cartItems: state.subCategory.cartItems
-         };
+        checkout: state.checkoutReducers.checkout,
+        cartItems: state.subCategory.cartItems
+    };
 }
 
-function mapDispatchToProps(dispatch)
-{
+function mapDispatchToProps(dispatch) {
     return {
-        actions: bindActionCreators({      
-            orderRequest  
+        actions: bindActionCreators({
+            orderRequest
         }, dispatch),
     };
 }
 
 export default connect(
-        mapStateToProps,
-        mapDispatchToProps
-        )(CheckoutPage);
+    mapStateToProps,
+    mapDispatchToProps
+)(CheckoutPage);
