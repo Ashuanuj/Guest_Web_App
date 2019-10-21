3c0eb0a3fc812f32d9669d755b9c19f626225943
Ashuanuj <ashutoshchoubeyanuj@gmail.com>
2019-10-21 10:29:50 +0530

    date format and mozilla scrollbar

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -193,7 +193,7 @@ class CheckoutPage extends React.Component {
             </Table>
             <div className="note-text-checkout">
               <FormGroup>
-              <Input type="textarea" rows="4" component={TextInput} name="Instructions" placeholder="Instructions ? E.g.Don’ t ring the doorbell"  onChange={this.onChange.bind(this)}  />
+              <Input type="textarea" rows="4" component={TextInput} name="Instructions" placeholder="Instructions ? E.g.Don’t ring the doorbell"  onChange={this.onChange.bind(this)}  />
               </FormGroup>
             </div>
             <Table className="bill-amt">
