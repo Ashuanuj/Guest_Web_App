ade6961a3b3c0252f4bb1c6be330ee7a07bd79a0
Ashuanuj <ashutoshchoubeyanuj@gmail.com>
2019-10-21 10:49:20 +0530

    deployed

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -55,7 +55,7 @@ class RequestForm extends React.Component{
           <Field component={TextInput} name="name" label="Name" onChange={()=>this.changeData(1)} />
         </FormGroup>
 
-        <FormGroup style={{border:this.props.values.roomno && this.state[`error${2}`] == undefined ? '' : !this.props.values.roomno && (this.props.error.customMessage.indexOf('Room') !== -1 || this.props.error.customMessage.indexOf('Invalid') !== -1 || a==true) ? '1px solid red' : a==true?'1px solid red': this.props.values.roomno && (this.props.error.customMessage.indexOf('Room') !== -1 || this.props.error.customMessage.indexOf('Invalid') !== -1 || a==true) ? '1px solid red' :
+        <FormGroup style={{border:this.props.values.roomno && this.state[`error${2}`] == undefined ? '' : !this.props.values.roomno && ( this.props.error && this.props.error.customMessage.indexOf('Room') !== -1 || this.props.error.customMessage.indexOf('Invalid') !== -1 || a==true) ? '1px solid red' : a==true?'1px solid red': this.props.values.roomno && (this.props.error.customMessage.indexOf('Room') !== -1 || this.props.error.customMessage.indexOf('Invalid') !== -1 || a==true) ? '1px solid red' :
           this.props.error && (this.props.error.customMessage.indexOf('Room') !== -1 || this.props.error.customMessage.indexOf('Invalid') !== -1 || a==true) 
           ?'1px solid red':''}}>
           <Field component={TextInput} name="roomno" label="Room No." onChange={()=>this.changeData(2)} />
